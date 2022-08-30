54fa02c15075e7509d119f4352d7d64c9c3374f6
vik37 <59177404+vik37@users.noreply.github.com>
2022-08-30 18:24:11 +0200

    Install XUnit test

---
+++ DemoLirary.Test/CalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DemoLirary.Test
+{
+    public class CalculatorTest
+    {
+        public void Add_SimpleValuesShouldCalculate()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+            
+        }
+    }
+}
